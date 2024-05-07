a8b1bb2dd409b158053ff5c9a58a9fb7d35e2473
Henry Allen <31718268+hmallen99@users.noreply.github.com>
2024-05-07 13:42:28 -0400

    Implement fireEvent and createEvent (#13)
    * Implement events
    
    * Add tests
    
    * Ensure export
    
    * Add wheel observable and more tests

---
+++ src/event.ts
@@ -0,0 +1,73 @@
+import { Observable } from '@babylonjs/core';
+import { Control } from '@babylonjs/gui';
+import { EventMap, eventMap } from './eventMap';
+
+export type Event<K extends keyof EventMap> = {
+    key: K;
+    observableName: EventMap[K]['observableName'];
+    eventData: EventMap[K]['defaultInit'];
+};
+
+function fireEventFn<K extends keyof EventMap>(
+    control: Control,
+    event: Event<K>
+) {
+    const eventObservable = control[event.observableName];
+
+    if (!(eventObservable instanceof Observable)) {
+        throw new Error(
+            `Unable to fire an event - event of type "${
+                event.key
+            }" does not exist on "${control.getClassName()}"`
+        );
+    }
+
+    (eventObservable as Observable<EventMap[K]['defaultInit']>).notifyObservers(
+        event.eventData
+    );
+}
+
+function createEventFn<K extends keyof EventMap>(
+    eventName: K,
+    _control: Control,
+    init: EventMap[K]['defaultInit']
+): Event<K> {
+    const event = eventMap[eventName];
+    return {
+        key: eventName,
+        observableName: event.observableName,
+        eventData: init,
+    };
+}
+
+Object.keys(eventMap).forEach((key: keyof EventMap) => {
+    const { defaultInit } = eventMap[key];
+    createEventFn[key] = (node: Control, init?: typeof defaultInit) =>
+        createEventFn(key, node, init ?? defaultInit);
+
+    fireEventFn[key] = (node: Control, init?: typeof defaultInit) => {
+        fireEventFn(node, createEventFn[key](node, init));
+    };
+});
+
+type CreateEventFn = (
+    k: keyof EventMap,
+    c: Control,
+    i: EventMap[keyof EventMap]['defaultInit']
+) => Event<keyof EventMap>;
+
+type CreateEventObject = {
+    [K in keyof EventMap]: (
+        c: Control,
+        i?: EventMap[K]['defaultInit']
+    ) => Event<K>;
+};
+
+type fireEventFn = (c: Control, e: Event<keyof EventMap>) => void;
+
+type fireEventObject = {
+    [K in keyof EventMap]: (c: Control, i?: EventMap[K]['defaultInit']) => void;
+};
+
+export const createEvent = createEventFn as CreateEventFn & CreateEventObject;
+export const fireEvent = fireEventFn as fireEventFn & fireEventObject;
